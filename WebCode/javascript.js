61473f5b2ac85ed84eeba0286a2ebed464648131
Jorgedlv <94132382+Jorgedlv@users.noreply.github.com>
2023-06-02 15:40:43 +0200

    Add files via upload

---
+++ WebCode/javascript.js
@@ -0,0 +1,14 @@
+const linkHistory = () => {
+    window.location.href = 'history.html'
+}
+
+const linkData = () => {
+    window.location.href = 'data.html'
+}
+
+const linkNews= () => {
+    window.location.href = 'news.html'
+}
+const linkHome= () => {
+    window.location.href = 'home.html'
+}
